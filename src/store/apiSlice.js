366d7d321fb2a2da042ecd3effcb3ed37250c0f9
arjun-singh-sudo <arjun-singh@pearlorganisation.com>
2026-02-24 10:16:50 +0530

    changes

--- src/store/apiSlice.js
+++ src/store/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:4040/api/v1",
+    baseUrl: process.env.NEXT_PUBLIC_API_URL,
     prepareHeaders: (headers) => {
       if (typeof window !== "undefined") {
         const token = localStorage.getItem("token");
